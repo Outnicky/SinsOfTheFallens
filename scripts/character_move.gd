15f6834926a90d331a8fcbcac6e989c0b01990be
ninjaskm <trzrazy@gmail.com>
2026-01-30 13:02:32 -0300

    mapa daora

--- scripts/character_move.gd
+++ scripts/character_move.gd
@@ -7,6 +7,8 @@ const Dash_Speed = 900.0
 var double_jump = true
 var dashing = false 
 var can_dash = true	
+var is_attacking : bool = false 
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,9 +16,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	var direction = 0
 	var timer = $Timer
-	
-	#var dash = 4
-	#var dashing = true 
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
@@ -26,54 +25,49 @@ func _physics_process(delta: float) -> void:
 		elif double_jump:
 			double_jump = false
 			velocity.y = JUMP_VELOCITY
-	
-	
 		
 	if Input.is_action_pressed("left"):
 		direction = -1
 	if Input.is_action_pressed("right"):
 		direction = 1
+	
 	if Input.is_action_just_pressed("dash") and can_dash:
 		can_dash = false
 		dashing = true
 		$dash_timer.start()	
 		$dash_again_timer.start()	
+		
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true 
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if direction:
 		if dashing :
 			velocity.x = direction * Dash_Speed 
 		else:
 			velocity.x = direction * SPEED
 		
-		
-		
-			
-			
 	else:
 		velocity.x = 0
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 
 	move_and_slide()
-
-
-#func _on_timer_timeout() -> void: 
 	
-	#pass # Replace with function body.
-
-
 func _on_dash_timer_timeout() -> void:
 	dashing = false
 	
-
-
 func _on_dash_again_timer_timeout() -> void:
 	can_dash = true
-
-
+	
 func _on_sword_hit_area_entered(area: Area2D) -> void:
 	if area.is_in_group("hurtBox"):
 		area.take_damage()
 		
+
+
+func _on_area_sword_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body is EnemyClass:
+		print(body.name)
+	
+			
